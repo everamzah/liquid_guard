e2f40cddececd3351b6e7904c404f8f8c9852959
James Stevenson <everamzah@gmail.com>
2016-07-09 11:47:32 -0400

    Add removal

--- init.lua
+++ init.lua
@@ -1,3 +1,28 @@
-minetest.override_item("default:lava_source", {})
-minetest.override_item("default:water_source", {})
-minetest.override_item("default:river_water_source", {})
+minetest.register_privilege("liquid", "Can place liquid source nodes.")
+
+minetest.override_item("default:lava_source", {
+	after_place_node = function(pos, placer, itemstack, pointed_thing)
+		if not minetest.check_player_privs(placer:get_player_name(),
+				{liquid = true}) then
+			minetest.remove_node(pos)
+		end
+	end,
+})
+
+minetest.override_item("default:water_source", {
+	after_place_node = function(pos, placer, itemstack, pointed_thing)
+		if not minetest.check_player_privs(placer:get_player_name(),
+				{liquid = true}) then
+			minetest.remove_node(pos)
+		end
+	end,
+})
+
+minetest.override_item("default:river_water_source", {
+	after_place_node = function(pos, placer, itemstack, pointed_thing)
+		if not minetest.check_player_privs(placer:get_player_name(),
+				{liquid = true}) then
+			minetest.remove_node(pos)
+		end
+	end,
+})
